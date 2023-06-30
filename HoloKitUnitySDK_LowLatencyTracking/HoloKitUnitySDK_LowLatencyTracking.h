9a7c59bc4c3828719bbb1d324de42e65c180b3c8
yuchen <yuchenz27@outlook.com>
2023-06-30 08:38:50 +0800

    Initial Commit

---
+++ HoloKitUnitySDK_LowLatencyTracking/HoloKitUnitySDK_LowLatencyTracking.h
@@ -0,0 +1,12 @@
+//
+//  HoloKitUnitySDK_LowLatencyTracking.h
+//  HoloKitUnitySDK_LowLatencyTracking
+//
+//  Created by Yuchen Zhang on 2023/6/30.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HoloKitUnitySDK_LowLatencyTracking : NSObject
+
+@end
---
+++ HoloKitUnitySDK_LowLatencyTracking/HoloKitUnitySDK_LowLatencyTracking.m
@@ -0,0 +1,12 @@
+//
+//  HoloKitUnitySDK_LowLatencyTracking.m
+//  HoloKitUnitySDK_LowLatencyTracking
+//
+//  Created by Yuchen Zhang on 2023/6/30.
+//
+
+#import "HoloKitUnitySDK_LowLatencyTracking.h"
+
+@implementation HoloKitUnitySDK_LowLatencyTracking
+
+@end
